a4f1215e88fdcbac981db438fade18ceb3b1153a
Alan <allan_net@live.com>
2022-04-23 07:59:30 -0300

    feat: dashboard component

--- src/presentation/components/table/table.tsx
+++ src/presentation/components/table/table.tsx
@@ -1,38 +1,13 @@
 import React from "react";
 import { alpha, styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import { MdEdit, MdDelete } from 'react-icons/md';
-import Tooltip from "@mui/material/Tooltip";
 import Styles from './table-styles.scss'
-import { DataGrid, gridClasses, GridColDef, GridRenderCellParams, GridValueFormatterParams, ptBR } from '@mui/x-data-grid';
-import { format } from 'date-fns'
+import { DataGrid, gridClasses, ptBR } from '@mui/x-data-grid';
 
-const TableComponent = ({ rows = [] }) => {
+type Props = { [key: string]: any }
 
-  function getAppointmentDate(params) {
-    return format(new Date(params.row.appointment_date), 'dd/MM/yyyy');
-  }
+const TableComponent = (props: Props) => {
 
-  function getAppointmentTime(params) {
-    return format(new Date(params.row.appointment_date),'HH:mm');
-  }
-
-  function getStatusTranslated(params) {
-    let message = ''
-    switch (params.row.status) {
-      case 'NOT VACCINED':
-        message = 'NÃO ATENDIDO'
-        break;
-      case 'VACCINED':
-        message = 'ATENDIDO'
-        break;
-      default:
-        message = params.row.status
-    }
-    return message
-}
-
-  const ODD_OPACITY = 0.2;
+const ODD_OPACITY = 0.2;
 
 const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
   [`& .${gridClasses.row}.even`]: {
@@ -66,80 +41,6 @@ const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
   },
 }));
 
-  const columns: GridColDef[] = [
-    {
-      field: 'actions',
-      headerName: 'Ações',
-      flex: 0.5,
-      sortable: false,
-      disableColumnMenu: true,
-      headerAlign: 'center',
-      align: 'center',
-      renderCell: (params: GridRenderCellParams<Date>) => (
-        <>
-          <Tooltip title="Editar">
-            <IconButton>
-              <MdEdit className={Styles.editIcon}/>
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Excluir">
-            <IconButton>
-              <MdDelete className={Styles.deleteIcon}/>
-            </IconButton>
-          </Tooltip>
-        </>
-      ),
-    },
-    {
-      field: 'name',
-      headerName: 'Nome',
-      minWidth: 200,
-      flex: 1,
-      disableColumnMenu: true,
-      headerAlign: 'center',
-      align: 'center',
-    },
-    {
-      field: 'birthday',
-      headerName: 'Data de Nascimento',
-      type: 'date',
-      flex: 0.5,
-      disableColumnMenu: true,
-      headerAlign: 'center',
-      align: 'center',
-      valueFormatter: (params: GridValueFormatterParams) => {
-        return format(new Date(params.value), 'dd/MM/yyyy');
-      },
-    },
-    {
-      field: 'appointment_date_day',
-      headerName: 'Data de Agendamento',
-      flex: 0.5,
-      disableColumnMenu: true,
-      headerAlign: 'center',
-      align: 'center',
-      valueGetter: getAppointmentDate,
-    },
-    {
-      field: 'appointment_date_hour',
-      headerName: 'Horário de Agendamento',
-      flex: 0.5,
-      headerAlign: 'center',
-      disableColumnMenu: true,
-      align: 'center',
-      valueGetter: getAppointmentTime,
-    },
-    {
-      field: 'status-translated',
-      headerName: 'Status',
-      headerAlign: 'center',
-      flex: 0.5,
-      disableColumnMenu: true,
-      align: 'center',
-      valueGetter: getStatusTranslated,
-    }
-  ];
-
   return (
     <div className={Styles.formRoot}>
       <div className={Styles.formContent}>
@@ -147,11 +48,10 @@ const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
           <StripedDataGrid
             localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
             autoHeight
-            rows={rows}
-            columns={columns}
+            rows={props.rows ? props.rows : []}
+            columns={props.columns}
             pageSize={15}
             rowsPerPageOptions={[15]}
-            //hideFooter={true}
             getRowClassName={(params) =>
               params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
             }
--- src/presentation/pages/dashboard/dashboard.tsx
+++ src/presentation/pages/dashboard/dashboard.tsx
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react"
 import { DeleteAppointment, LoadAppointments } from "@/domain/usecases"
-import { Header, Footer, Table } from "@/presentation/components"
+import { Header, Footer, Table, Snackbar, AlertDialog } from "@/presentation/components"
 import Styles from './dashboard-styles.scss'
+import IconButton from '@mui/material/IconButton';
+import { MdEdit, MdDelete } from 'react-icons/md';
+import Tooltip from "@mui/material/Tooltip";
+import { GridColDef, GridRenderCellParams, GridValueFormatterParams } from '@mui/x-data-grid';
+import { format } from 'date-fns'
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import { useHistory } from "react-router-dom";
 
 type Props = {
   loadAppointments: LoadAppointments
   deleteAppointment: DeleteAppointment
 }
 
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const Dashboard: React.FC<Props> = ({ loadAppointments, deleteAppointment }: Props) => {
   const [appointmentsList, setAppointmentsList] = useState([])
+  const history = useHistory()
 
   useEffect(() => {
     loadAppointments.loadAll().then((appointments) => {
@@ -17,13 +32,174 @@ const Dashboard: React.FC<Props> = ({ loadAppointments, deleteAppointment }: Pro
     })
   }, [])
 
+  function getAppointmentDate(params) {
+    return format(new Date(params.row.appointment_date), 'dd/MM/yyyy');
+  }
+
+  function getAppointmentTime(params) {
+    return format(new Date(params.row.appointment_date), 'HH:mm');
+  }
+
+  function getStatusTranslated(params) {
+    let message = ''
+    switch (params.row.status) {
+      case 'NOT VACCINED':
+        message = 'NÃO ATENDIDO'
+        break;
+      case 'VACCINED':
+        message = 'ATENDIDO'
+        break;
+      default:
+        message = params.row.status
+    }
+    return message
+  }
+
+  /*
+  function openDeleteDialog(appointment) {
+    setCurrentAppointment(appointment)
+    setDeleteDialog(true)
+  }
+
+  function closeDeleteDialog() {
+    setDeleteDialog(false)
+  }
+  */
+
+  const columns: GridColDef[] = [
+    {
+      field: 'actions',
+      headerName: 'Ações',
+      flex: 0.4,
+      sortable: false,
+      disableColumnMenu: true,
+      headerAlign: 'center',
+      align: 'center',
+      renderCell: (params: GridRenderCellParams<Date>) => (
+        <>
+          <Tooltip title="Editar">
+            <IconButton onClick={() => history.push(`/agendamento/${params.row.id}`)}>
+              <MdEdit className={Styles.editIcon} />
+            </IconButton>
+          </Tooltip>
+          {/*}
+          <Tooltip title="Excluir">
+            <IconButton onClick={() => openDeleteDialog(params.row)}>
+              <MdDelete className={Styles.deleteIcon} />
+            </IconButton>
+          </Tooltip>
+      */}
+        </>
+      ),
+    },
+    {
+      field: 'name',
+      headerName: 'Nome',
+      minWidth: 200,
+      flex: 1,
+      disableColumnMenu: true,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    {
+      field: 'birthday',
+      headerName: 'Data de Nascimento',
+      type: 'date',
+      flex: 0.5,
+      disableColumnMenu: true,
+      headerAlign: 'center',
+      align: 'center',
+      valueFormatter: (params: GridValueFormatterParams) => {
+        return format(new Date(params.value), 'dd/MM/yyyy');
+      },
+    },
+    {
+      field: 'appointment_date_day',
+      headerName: 'Data de Agendamento',
+      flex: 0.5,
+      disableColumnMenu: true,
+      headerAlign: 'center',
+      align: 'center',
+      valueGetter: getAppointmentDate,
+    },
+    {
+      field: 'appointment_date_hour',
+      headerName: 'Horário de Agendamento',
+      flex: 0.5,
+      headerAlign: 'center',
+      disableColumnMenu: true,
+      align: 'center',
+      valueGetter: getAppointmentTime,
+    },
+    {
+      field: 'status-translated',
+      headerName: 'Status',
+      headerAlign: 'center',
+      flex: 0.5,
+      disableColumnMenu: true,
+      align: 'center',
+      valueGetter: getStatusTranslated,
+    }
+  ];
+
+  /*const handleDeleteSubmit = async (appointment_id: string): Promise<void> => {
+    console.log(appointment_id)
+    try {
+      await deleteAppointment.delete()
+      closeDeleteDialog()
+      setRefresh((prevValues) => prevValues + 1)
+      setSuccessMessage(true)
+    } catch (error) {
+      closeDeleteDialog()
+      setErrorMessage(true);
+    }
+  }
+  */
+
+  /*
+  const handleSuccessClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSuccessMessage(false);
+  }
+
+  const handleErrorClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setErrorMessage(false);
+  }
+  */
+
   return (
     <div className={Styles.root}>
       <div className={Styles.headerBase}>
         <Header />
       </div>
       <div className={Styles.centerBase}>
-        <Table rows={appointmentsList}/>
+        <Table
+          rows={appointmentsList}
+          columns={columns}
+        />
+        {/*
+        <Snackbar 
+        successMessage={successMessage}
+        setSuccessMessage={setSuccessMessage}
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+        handleErrorClose={handleErrorClose}
+        handleSuccessClose={handleSuccessClose}
+        />
+        <AlertDialog
+          dialogStatus={deleteDialog}
+          openDialog={openDeleteDialog}
+          closeDialog={closeDeleteDialog}
+          handleConfirm={() => handleDeleteSubmit(currentAppointment.id)}
+          title={'Deseja realmente excluir o agendamento?'}
+          message={`O agendamento de ${currentAppointment.name} será permanentemente excluido do sistema`}
+        />
+         */}
       </div>
       <div className={Styles.footerBase}>
         <Footer />
