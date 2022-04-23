78c889c8b93a99b33f0c2024724357b5c3c43128
Alan <allan_net@live.com>
2022-04-23 09:09:27 -0300

    feat: add cdn to external depencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,7 +29,10 @@ module.exports = merge (common, {
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM'
+    axios: 'axios',
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM'
+    
   },
   plugins: [
     new DefinePlugin({
