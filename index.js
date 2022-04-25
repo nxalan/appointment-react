8ed5a686295f1393387dd0a85af03fef020e7b8b
Alan <allan_net@live.com>
2022-04-24 23:38:01 -0300

    docs: add documentation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fallback = require('express-history-api-fallback')
 const express = require('express')
 const { join } = require('path')
+const helmet = require("helmet");
 
 const app = express()
 const root = join(__dirname, 'dist')
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
+app.use(helmet())
 app.listen(process.env.PORT || 3000)
